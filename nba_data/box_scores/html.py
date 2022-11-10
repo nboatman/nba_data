6ba7c0f51eddc907e4c925c155f337b354f2d1f3
nboatman <boatmanns@gmail.com>
2022-11-10 02:01:30 -0600

    Use relative url when getting boxscore

--- nba_data/box_scores/html.py
+++ nba_data/box_scores/html.py
@@ -1,7 +1,10 @@
 import requests
 from lxml import html
 
+BASE_URL = 'https://www.basketball-reference.com'
 
-def get_boxscore_html(url):
-    response =  requests.get(url=url, allow_redirects=False)
+
+def get_boxscore_html(relative_url):
+    absolute_url = f"{BASE_URL}{relative_url}"
+    response =  requests.get(url=absolute_url, allow_redirects=False)
     return html.fromstring(response.content)
