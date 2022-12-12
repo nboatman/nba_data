1a5323cc56c8d9e2bf8958087a8eddd1e558f270
nboatman <boatmanns@gmail.com>
2022-12-12 09:26:04 -0600

    Add team mappings and make_predictions function to pace model
    - Team mappings should be moved later.

--- nba_data/models/pace_by_avg.py
+++ nba_data/models/pace_by_avg.py
@@ -10,6 +10,17 @@ MODEL_NAME = 'Predict pace by averaging recent averages'
 DATA_HANDLER = NBAData()
 LOOK_BACK_GAMES = 25
 
+FD_TO_BR = {
+ 'ATL': 'ATL', 'BKN': 'BRK', 'BOS': 'BOS', 'CHA': 'CHO',
+ 'CHI': 'CHI', 'CLE': 'CLE', 'DAL': 'DAL', 'DEN': 'DEN',
+ 'DET': 'DET', 'GS': 'GSW', 'HOU': 'HOU', 'IND': 'IND',
+ 'LAC': 'LAC', 'LAL': 'LAL', 'MEM': 'MEM', 'MIA': 'MIA',
+ 'MIL': 'MIL', 'MIN': 'MIN', 'NO': 'NOP', 'NY': 'NYK',
+ 'OKC': 'OKC', 'ORL': 'ORL', 'PHI': 'PHI', 'PHO': 'PHO',
+ 'POR': 'POR', 'SA': 'SAS', 'SAC': 'SAC', 'TOR': 'TOR',
+ 'UTA': 'UTA', 'WAS': 'WAS'
+}
+
 
 class Model(BaseModel):
     def __init__(self):
@@ -41,3 +52,39 @@ class Model(BaseModel):
         filtered = filtered[[('pace', 'mean'), ('pace_recent', 'mean')]].droplevel(1, axis=1)
 
         return filtered
+
+    def make_predictions(self, slate_id, game_date):
+        sql = f"""select game_id, team, pace
+                  from {DATA_HANDLER.four_factors.name}
+                  where game_id < '{game_date}'
+                  order by game_id;"""
+
+        with sqlite3.connect(nba_database) as conn:
+            pace_df = pd.read_sql(sql, conn, index_col=['game_id'])
+
+        pace_team_tmp = pace_df[['team'] + json.loads(self.target_cols)] \
+            .groupby('team') \
+            .rolling(LOOK_BACK_GAMES, closed='left') \
+            .mean() \
+            .reset_index()
+
+        recent_pace = pace_team_tmp.groupby('team').last()
+
+        games_sql = f"""select distinct game_id, team, opponent
+                        from {DATA_HANDLER.fanduel_player_list.name}
+                        where game_id = '{slate_id}';"""
+
+        with sqlite3.connect(nba_database) as conn:
+            games_df = pd.read_sql(games_sql, conn)
+
+        games_df['team'] = games_df['team'].map(FD_TO_BR)
+        games_df['opponent'] = games_df['opponent'].map(FD_TO_BR)
+
+        games_df['team_pace'] = games_df['team'].map(lambda x: recent_pace.loc[x, 'pace'])
+        games_df['opponent_pace'] = games_df['opponent'].map(lambda x: recent_pace.loc[x, 'pace'])
+
+        games_df['pace_recent'] = 1/2 * (games_df['team_pace'] + games_df['opponent_pace'])
+
+        predictions = self.model.predict(games_df[['pace_recent']])
+        games_df['pace_predicted'] = predictions
+        return games_df
