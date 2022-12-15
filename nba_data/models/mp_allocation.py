0c133434cfa0c4f13dbf791a24606148c66ec835
nboatman <boatmanns@gmail.com>
2022-12-15 02:14:31 -0600

    Update model for mp allocation

--- nba_data/models/mp_allocation.py
+++ nba_data/models/mp_allocation.py
@@ -15,7 +15,7 @@ class Model(BaseModel):
     def __init__(self):
         super().__init__(MODEL_ID, MODEL_NAME)
         self.model = LinearRegression()
-        self.predictor_cols = json.dumps(['bpm_recent', 'team_bpm_recent'])
+        self.predictor_cols = json.dumps(['mp_proportion_recent', 'is_starter', 'is_starter_recent'])
         self.target_cols = json.dumps(['mp_proportion'])
 
     def get_historical_data(self):
@@ -32,27 +32,20 @@ class Model(BaseModel):
             team_minutes_df = pd.read_sql(team_minutes_sql, conn, index_col=['game_id', 'team'])
             player_minutes_df = pd.read_sql(player_minutes_sql, conn, index_col=['game_id', 'team'])
 
-        player_avg_bpm = player_minutes_df[['id', 'bpm']]\
-            .groupby('id')\
-            .rolling(LOOK_BACK_GAMES, min_periods=4)\
-            .mean()
-
         player_minutes_df = player_minutes_df.merge(team_minutes_df, on=['game_id', 'team'], suffixes=('', '_team'))
-        player_minutes_df = player_minutes_df.reset_index('team').merge(player_avg_bpm, on=['game_id', 'id'], suffixes=('', '_recent'))
         player_minutes_df['mp_proportion'] = player_minutes_df['mp'] / player_minutes_df['mp_team']
-        player_minutes_df['weighted_bpm'] = player_minutes_df['bpm'] * player_minutes_df['mp_proportion']
 
-        team_bpm = player_minutes_df[['team', 'weighted_bpm']]\
-            .groupby(['game_id', 'team'])\
-            .sum()
+        player_minutes_df.sort_index(inplace=True)
 
-        team_recent_bpm = team_bpm.reset_index('team')\
-            .groupby('team')\
-            .rolling(LOOK_BACK_GAMES)\
-            .mean()
+        recent_mp = player_minutes_df[['id', 'mp_proportion', 'is_starter']] \
+            .groupby('id') \
+            .rolling(LOOK_BACK_GAMES, min_periods=5, closed='left') \
+            .mean().fillna(0)
 
-        player_minutes_df['team_bpm'] = player_minutes_df.merge(team_bpm, on=['game_id', 'team'], suffixes=('', '_team'))['weighted_bpm_team']
+        player_minutes_df = player_minutes_df.merge(
+            recent_mp,
+            on=['id', 'team', 'game_id'],
+            suffixes=('', '_recent')
+        ).reset_index().dropna()
 
-        player_minutes_df['team_bpm_recent'] = player_minutes_df.merge(team_recent_bpm, on=['game_id', 'team'], suffixes=('', '_recent'))['weighted_bpm_recent']
-
-        return player_minutes_df[player_minutes_df['mp'] > 10], team_bpm, team_recent_bpm
+        return player_minutes_df[player_minutes_df['game_id'] > '202002']
